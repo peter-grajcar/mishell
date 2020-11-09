6649e6c208ce2d264cbb8641136bed912ea8a7d2
Peter Grajcar <peter.grajcar131@gmail.com>
2020-11-09 12:45:59 +0100

    add comments
    Signed-off-by: Peter Grajcar <peter.grajcar131@gmail.com>

--- src/context.c
+++ src/context.c
@@ -4,24 +4,42 @@
 
 static int lineno;
 
+/*
+ * Initialises line number to zero.
+ */
 void
 ctx_init(void)
 {
 	lineno = 0;
 }
 
+/*
+ * Returns the current line number.
+ *
+ * @return current line number.
+ */
 int
 ctx_line(void)
 {
 	return (lineno);
 }
 
+/*
+ * Increments the currrent line number.
+ *
+ * @return incremented line number.
+ */
 int
 ctx_incline(void)
 {
 	return (lineno++);
 }
 
+/*
+ * Prints lexer and parser error messages. This function is required by bison.
+ *
+ * @param fmt format.
+ */
 void
 yyerror(const char *fmt, ...)
 {
